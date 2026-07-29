feafd8cb2e3edfbf258c75223631e6b185379a8d
yasmin-xyz <yasmindoust@gmail.com>
2026-07-28 22:14:01 -0400

    Shrink nav logo further: 31px/23px -> 27px/20px (desktop/mobile)

--- app/components/HomeLogoLink.tsx
+++ app/components/HomeLogoLink.tsx
@@ -20,8 +20,8 @@ export default function HomeLogoLink() {
       <Image
         src="/android-chrome-192x192.png"
         alt="Pick'em Labs"
-        width={31}
-        height={31}
+        width={27}
+        height={27}
         className="nav-logo-img"
       />
       <div className="nav-logo-text">
